7f04d8625fa2cecd5ff0b2e5f9143c760bc51f06
qianyue <zhangyunpu@arxanfintech.com>
2018-06-29 12:21:30 +0800

    简单修改装饰器

--- decorator/__init__.py
+++ decorator/__init__.py
@@ -1,5 +1,6 @@
 # -*- encoding:utf-8 -*-
 # create by Administrator on 2018/6/25
+from functools import wraps
 
 """
  装饰器
@@ -13,3 +14,20 @@ def eval_now(func):
     :return:
     """
     return func()
+
+
+def repeat(count):
+    """
+    重复执行指定函数的装饰器
+    :param count:
+    :return:
+    """
+    def wrapper1(func):
+        @wraps(func)
+        def wrapper2(*args, **kwargs):
+            for i in range(count):
+                func(args, kwargs)
+
+        return wrapper2
+
+    return wrapper1
